f8b100749b0ae602afa191f2fd7a33dc1d1acf3b
Tochie <tochiswork@gmail.com>
2019-09-20 23:58:24 +0100

    Pascal Triangle

---
+++ src/com/Algorithms/pascalTriangle.java
@@ -0,0 +1,41 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.Algorithms;
+
+public class pascalTriangle {
+  public pascalTriangle() {
+  }
+
+  public static void main(String[] args) {
+    triangle();
+  }
+
+  private static void triangle() {
+    int n = 4;
+    int[][] triangle = new int[n][];
+
+    int r;
+    int c;
+    for(r = 0; r < n; ++r) {
+      triangle[r] = new int[r + 1];
+      triangle[r][0] = 1;
+      triangle[r][r] = 1;
+
+      for(c = 1; c < r; ++c) {
+        triangle[r][c] = triangle[r - c][c - 1] + triangle[r - 1][c];
+      }
+    }
+
+    for(r = 0; r < triangle.length; ++r) {
+      for(c = 0; c < triangle[r].length; ++c) {
+        System.out.printf("%4d", triangle[r][c]);
+      }
+
+      System.out.println();
+    }
+
+  }
+}
