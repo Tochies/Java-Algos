44ef0497410be84c5f4d909040f56744d3cb5e8a
Tochi Emeasoba <tochiswork@gmail.com>
2022-09-07 07:20:55 +0100

    implement sum of diagonal matrix

---
+++ src/main/com/Exercises/HackerRank/DiagonalDifference/Result.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2022.
+ *
+ * @author Tochie
+ *
+ * Date 9/7/22, 7:17 AM
+ */
+
+package com.Exercises.HackerRank.DiagonalDifference;
+
+import java.util.List;
+
+/**
+ *
+ Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+ For example, the square matrix  is shown below:
+
+ 1 2 3
+ 4 5 6
+ 9 8 9
+ The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+ Function description
+
+ Complete the  function in the editor below.
+
+ diagonalDifference takes the following parameter:
+
+ int arr[n][m]: an array of integers
+ Return
+
+ int: the absolute diagonal difference
+ Input Format
+
+ The first line contains a single integer, , the number of rows and columns in the square matrix .
+ Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+ Constraints
+
+ Output Format
+
+ Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+ Sample Input
+
+ 3
+ 11 2 4
+ 4 5 6
+ 10 8 -12
+ Sample Output
+
+ 15
+ Explanation
+
+ The primary diagonal is:
+
+ 11
+ 5
+ -12
+ Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+ The secondary diagonal is:
+
+ 4
+ 5
+ 10
+ Sum across the secondary diagonal: 4 + 5 + 10 = 19
+ Difference: |4 - 19| = 15
+
+ Note: |x| is the absolute value of x
+ *
+ *
+ *
+ * **/
+public class Result {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int j = arr.get(0).size();
+        int i = 0;
+        int a = 0;
+        int b = 0;
+
+        while( i < j){
+            a += arr.get(i).get(i);
+            b +=  arr.get(i).get(j-1-i);
+
+            i ++;
+        }
+
+        return Math.abs(a-b);
+    }
+
+}
